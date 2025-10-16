094edd0739b886386a22e7fc7b5cc75dac2e8a81
AbhiPatil20072003 <abhishekpatil2805@gmail.com>
2025-10-16 11:01:33 +0530

    16th Oct Taskform Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+
 import Taskform from "./Components/Taskform"
 import TaskList from "./Components/TaskList"
 import Progresstracker from "./Components/Progresstracker"
-function App() {
-  
+import { useEffect, useState } from "react"
+
+export default function App() {
+  const[tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  });
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]);
+  }
 
   return (
     <>
       <h1>Task Manager</h1>
       <p>Our friendly task manager helps you organize your tasks efficiently.</p>
-      <Taskform />
+      <Taskform addTask={addTask} />
       <TaskList />
       <Progresstracker />
       <button>Clear All Tasks</button>
@@ -16,4 +27,4 @@ function App() {
   )
 }
 
-export default App
+
--- src/Components/Taskform.jsx
+++ src/Components/Taskform.jsx
@@ -1,23 +1,40 @@
-export default function Taskform() {
-  return( 
-    <form>
-        <div>
-            <input type="text" placeholder="Enter your task here"  />
-            <button type="submit">Add Task</button>
-        </div>
-        <div>
-            <select>
-                <option value="high">High</option>
-                <option value="medium">Medium</option>
-                <option value="low">Low</option>
-            </select>
+import { use, useState } from "react";
 
-            <select>
-                <option value="general">General</option>
-                <option value="work">Work</option>
-                <option value="personal">Personal</option>
-            </select>
-        </div>
-    </form> 
-  );
+export default function Taskform() {
+    const[task, setTask] = useState('');
+    const[priority, setPriority] = useState('medium');
+    const[category, setCategory] = useState('general');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTask({text: task, priority, category, completed: false});
+
+        //reset form
+        setTask('');
+        setPriority('medium');
+        setCategory('general');
+    }
+    return( 
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type="text" placeholder="Enter your task here" value={task} onChange={(e) => setTask(e.target.value)}/>
+                <button type="submit">Add Task</button>
+                <h1>{task} {priority} {category}</h1>
+            </div>
+
+            <div>
+                <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+                </select>
+            
+                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                    <option value="general">General</option>
+                    <option value="work">Work</option>
+                    <option value="personal">Personal</option>
+                </select>
+            </div>
+        </form> 
+    );
 }
